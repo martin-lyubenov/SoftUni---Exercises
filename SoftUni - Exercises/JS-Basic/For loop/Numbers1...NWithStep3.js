f8411ae63351aebbf90cdad2af96885f01658e89
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-08 14:19:50 +0300

    more homework

---
+++ SoftUni - Exercises/JS-Basic/For loop/Numbers1...NWithStep3.js
@@ -0,0 +1,8 @@
+function Numbers1NWithStep3(input) {
+    
+let num = Number(input[0]);
+
+for (let a = 1; a <= num; a= a+3 )
+    console.log(a);
+}
+Numbers1NWithStep3(["7"]);
---
+++ SoftUni - Exercises/JS-Basic/For loop/numbers1To100.js
@@ -0,0 +1,11 @@
+function numbers(input) {
+    
+let num = Number(input[0]);
+
+for (let a = num; a >=1; a--) {
+    console.log(a);
+}
+
+
+}
+numbers(["5"]);
