9b4e6a13a804b5ae356ed2313836252890a0beaf
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-12 13:04:07 +0300

    more homework

---
+++ SoftUni - Exercises/JS-Basic/Nested Loops/clock.js
@@ -0,0 +1,10 @@
+function clock() {
+    
+    for(let h = 0; h <=23; h++) { 
+        for(let min = 0; min <= 59; min++) { 
+            console.log(`${h}:${min}`);
+        }
+    }
+
+}
+clock();
